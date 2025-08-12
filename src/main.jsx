9268010bceba812d13f0aef47322e91dbc58c351
Africanspeakerbureau <Github@africanspeakerbureau.com>
2025-08-12 12:03:05 +0200

    feat: add global footer

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import App from './App.jsx'
 import FindSpeakersPage from './components/FindSpeakersPage.jsx'
 import SpeakerProfile from './components/SpeakerProfile.jsx'
 import { ToastProvider } from './components/Toast'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +20,8 @@ createRoot(document.getElementById('root')).render(
           <Route path="/book-a-speaker" element={<App />} />
           <Route path="/*" element={<App />} />
         </Routes>
+        {/* Global footer for all routes */}
+        <Footer />
       </Router>
     </ToastProvider>
   </StrictMode>,
