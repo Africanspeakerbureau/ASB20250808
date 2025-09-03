b50f23ae16540cc101d73af769e995ca0a988069
Africanspeakerbureau <Github@africanspeakerbureau.com>
2025-09-03 16:38:10 +0200

    fix: add SPA rewrite and auth deps

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
 import App from './App.jsx'
 import FindSpeakersPage from './components/FindSpeakersPage.jsx'
